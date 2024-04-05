2e3875cde62a1cb7c2b85a7a47b7de98f10e273a
Maksym Soloviov <42580651+soloveyko23@users.noreply.github.com>
2024-04-05 10:40:28 +0200

    Add files via upload

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -155,22 +155,103 @@ const initializeSlider = () => {
 };
 
 
-document.addEventListener("DOMContentLoaded", function() {
-  const widgets = document.querySelectorAll('.widget');
+let slideUp = (target, duration=500) => {
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.boxSizing = 'border-box';
+  target.style.height = target.offsetHeight + 'px';
+  target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout( () => {
+    target.style.display = 'none';
+    target.style.removeProperty('height');
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+    //alert("!");
+  }, duration);
+}
+
+let slideDown = (target, duration=500) => {
+  target.style.removeProperty('display');
+  let display = window.getComputedStyle(target).display;
+
+  if (display === 'none')
+    display = 'block';
+
+  target.style.display = display;
+  let height = target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = 'border-box';
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = height + 'px';
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+  window.setTimeout( () => {
+    target.style.removeProperty('height');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+  }, duration);
+}
+ let slideToggle = (target, duration = 500) => {
+  if (window.getComputedStyle(target).display === 'none') {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+}
+ 
+// ====  
+
+let speedAnimation = 400;
+let targetId = document.getElementById("target");
+const widgets = document.querySelectorAll('.widget');
   
-  widgets.forEach(function(widget) {
-    const closeButton = widget.querySelector('.widget-close');
-    if (closeButton) {
-      closeButton.addEventListener('click', function(event) {
-        widget.classList.toggle('hide')
-        const widgetBody = widget.querySelector('.widget-body');
-        if (widgetBody) {
-          slideToggle(widgetBody);
-        }
-      });
-    }
-  });
+widgets.forEach(function(widget) {
+  const closeButton = widget.querySelector('.widget-close');
+  if (closeButton) {
+    closeButton.addEventListener('click', function(event) {
+      widget.classList.toggle('hide')
+      const widgetBody = widget.querySelector('.widget-body');
+      if (widgetBody) {
+        slideToggle(widgetBody);
+      }
+    });
+  }
 });
+
+
+
+// =========== old
+
+//   document.getElementById("triggerUp").addEventListener('click', function() {
+//   slideUp(document.getElementById("target"), 400);
+// });
+//   document.getElementById("triggerDown").addEventListener('click', function() {
+//   slideDown(document.getElementById("target"), 400);
+// });
+//   document.getElementById("triggerToggle").addEventListener('click', function() {
+//   slideToggle(document.getElementById("target"), 400);
+// });
 
 document.addEventListener("DOMContentLoaded", () => {
   const hideButton = document.querySelector('.hide-widgets');
